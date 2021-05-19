c5d86aaab93d58061274e19cfd5f67a4ebd2e868
Konstantin <zkonstr@gmail.com>
2021-05-19 19:10:48 +0300

    core code added
    core code from edurfe has been added

---
+++ src/main/java/bsu/rfe/java/zhibul/entity/ChatMessage.java
@@ -0,0 +1,34 @@
+package bsu.rfe.java.zhibul.entity;
+
+public class ChatMessage {
+    // Текст сообщения
+    private String message;
+    // Автор сообщения
+    private ChatUser author;
+    // Временная метка сообщения (в микросекундах)
+    private long timestamp;
+    public ChatMessage(String message, ChatUser author, long timestamp) {
+        super();
+        this.message = message;
+        this.author = author;
+        this.timestamp = timestamp;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public ChatUser getAuthor() {
+        return author;
+    }
+    public void setAuthor(ChatUser author) {
+        this.author = author;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
